7db082dc7a2ccd5659606b102bc93cfba4c3de54
Kameron Pete <kameroncpete@gmail.com>
2024-02-29 08:32:08 -0800

    finished testing

---
+++ test.js
@@ -0,0 +1,26 @@
+/*
+Unit Tests of Multiplication Function:
+-expect numbers to work in the function
+-expect undefined if strings are trying to be multiplied
+-expect a number to be returned when the function is run
+-expect returned number to be the product of the integers inside the function
+*/
+
+/*
+Unit Tests of ConcatOdds Function:
+-expect return value to be an array of numbers
+-expect returned array to contain only odd numbers
+-expect returned array not to have any strings or boolean values
+-expect if no odd numbers are found that an empty string is returned
+-expect that if strings are in the inputed arrays that the function still runs, but ignores those values like it would ignore the even numbers.
+*/
+
+/*
+Functional Tests of Shopping Cart Checkout Feature:
+-when a user opens the cart and the cart is empty, there should be no button or prompt allowing the user to check out
+-when an item is placed in the cart the prompt allowing the user to checkout appears at the bottom of the cart
+-when the "checkout- im a member" button is pressed, user should be taken to the login page of the website
+-once the user is logged in, it should automatically take him to the checkout page
+-members information should be auto populated on the checkout page
+-when "check out as guest" is clicked, he is taken straight to the checkin page
+*/
